20049efef753758c8fd34a14e65708c2a3f1a4db
Rose Egle <rosiemegle89@gmail.com>
2020-09-11 14:13:16 -0400

    played around with jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let city = {
+  name: "Madrid",
+  country: "Spain"
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><h1 id="heading" className="cool-text">Hello from {city.name}, {city.country}!</h1>
+ <ul>
+   <li>Puppies</li>
+  <li> Unicorns</li>
+  <li>Rainbows!</li>
+ </ul>
+ </div>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
