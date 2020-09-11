14a67dce877241d1b06fc7a468ee6c325666bcd0
Rose Egle <rosiemegle89@gmail.com>
2020-09-11 18:03:20 -0400

    created list of pbjects to map through

--- src/index.js
+++ src/index.js
@@ -2,24 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let city = {
-  name: "Madrid",
-  country: "Spain"
+let lakeList = [
+ {id: "1", name: "Echo", trailhead: "Echo"},
+ {id: "2", name: "Maud", trailhead: "Wrights"},
+ {id: "3", name: "Velma", trailhead: "Bayview"}
+
+]
+
+function App({lakes}) {
+  return (
+    <div>
+      {lakes.map(lake => 
+      <div key={lake.id}>
+        <h2>{lake.name}</h2>
+        <p>Accessed by {lake.trailhead}</p>
+        </div>)}
+    </div>
+  )
 }
 
- let Hello = (props) => {
-   console.log(Object.keys(props).length)
-  return (
-  <div>
-  <h1>Welcome to the page of welcome!</h1>
-  <h2>{props.message}</h2>
-  <p>The place where unicorns rule!</p>
-  <p>Unicorns currently admitted: {props.number}</p>
-  </div>
-  )}
 
 ReactDOM.render(
-<Hello library="React" message="Only Unicorns allowed!" number={3}/>,
+<App lakes={lakeList}/>,
   document.getElementById('root')
 );
 
