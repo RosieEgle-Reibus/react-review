9db9273bc69e9e8a6d08fe2ffaaf73aa92a6d3a5
Rose Egle <rosiemegle89@gmail.com>
2020-09-14 15:08:47 -0400

    added mutltiple useState options

--- src/index.js
+++ src/index.js
@@ -4,16 +4,29 @@ import './index.css';
 
 
 let App = () => {
-
+  const [year, setYear] = useState(2050)
+  const [manger, setManger] = useState("The Unicorn")
   const [status, setStatus] = useState("Open") 
 
 return (
+  <>
+  <h1>The Unicorned</h1>
+  <h2>A place for unicorns to eat popcorn</h2>
+   <div>
+    <h2>{year}</h2>
+    <button onClick={() => setYear(year + 1)}>Next year</button>
+  </div>
   <div>
-    <h1>Status: {status}</h1>
+    <h2>Manager on Duty: {manger}</h2>
+    <button onClick={() => setManger("Unicorny")}>Change Manager</button>
+  </div>
+  <div>
+    <h3>Status: {status}</h3>
     <button onClick={() => setStatus("Open")}>Open</button>
     <button onClick={() => setStatus("Back in 5")}>Back in 5</button>
     <button onClick={() => setStatus("Closed")}>Closed</button>
   </div>
+  </>
 )
 }
 
